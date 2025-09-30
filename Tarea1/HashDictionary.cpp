005c483fbce6df9ed8c62b41bde4aa84f3e36177
SamuelGlz10 <a01412958@tec.mx>
2025-09-29 20:55:37 -0600

    Modificacion en base a leetcode

--- Tarea1/HashDictionary.cpp
+++ Tarea1/HashDictionary.cpp
@@ -4,25 +4,43 @@
 #include <unordered_map>
 // Incluye la librería para usar strings
 #include <string>
+// Incluye la librería para usar stack
+#include <stack>
 
 using namespace std;
 
-int main(){
-    // Declaración de un unordered_map que asocia enteros con strings
-    unordered_map<int, string> hashMap;
-    // Inicialización del hashMap con algunos valores
-    hashMap = {
-        {1, "uno"},
-        {2, "dos"},
-        {3, "Samuel"},
-        {4, "Gonzalez"},
-        {5, "Vazquez"}
+// Función que valida si una cadena de paréntesis es válida
+bool isValid(const string& s) {
+    stack<char> st;
+    unordered_map<char, char> hashmap = {
+        {')', '('},
+        {'}', '{'},
+        {']', '['}
     };
 
-    // Imprime todos los elementos del hashMap
-    cout << "Elementos en el hashMap:" << endl;
-    for (const auto& pair : hashMap) {
-        cout << pair.first << " : " << pair.second << endl;
+    for (char c : s) {
+        if (hashmap.count(c)) {
+            if (!st.empty() && st.top() == hashmap[c]) {
+                st.pop();
+            } else {
+                return false;
+            }
+        } else {
+            st.push(c);
+        }
     }
+    return st.empty();
+}
+
+int main(){
+
+    // Ejemplo de uso de isValid
+    string test1 = "()[]{}";
+    string test2 = "(]";
+    cout << "La cadena '" << test1 << "' es ";
+    cout << (isValid(test1) ? "valida" : "invalida") << endl;
+    cout << "La cadena '" << test2 << "' es ";
+    cout << (isValid(test2) ? "valida" : "invalida") << endl;
+
     return 0;
 }
